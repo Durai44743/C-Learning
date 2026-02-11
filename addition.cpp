86a68782c754a59098edda82137f8d851a237a02
Durai44743 <duraidurai44743@gmail.com>
2026-02-11 05:17:39 -0800

    addition

---
+++ addition.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a,b,c;
+    cout<<"Enter the Values:";
+    cin>>a>>b;
+    c = a+b;
+    cout<<"Add = "<<c<<endl;
+    return 0;
+}
